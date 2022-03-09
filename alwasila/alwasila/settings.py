d2e14a2cc24b2ae8f2f948488bd71efc232d4819
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-03-09 13:16:18 +0500

    fix static settings

--- alwasila/alwasila/settings.py
+++ alwasila/alwasila/settings.py
@@ -133,5 +133,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mainapp/media/')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static/'),
 )
