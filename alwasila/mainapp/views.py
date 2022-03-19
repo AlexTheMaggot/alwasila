f6d5d70ead345fc41f325cc7d78347fb671271f7
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-03-19 14:55:15 +0500

    add contacts view

--- alwasila/mainapp/views.py
+++ alwasila/mainapp/views.py
@@ -14,3 +14,8 @@ def index(request):
 def about(request):
     template = 'mainapp/about.html'
     return render(request, template)
+
+
+def contacts(request):
+    template = 'mainapp/contacts.html'
+    return render(request, template)
