944bc07f9d932c226ada57103a5d88021261a68c
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-03-24 23:10:41 +0500

    add Product model

--- alwasila/mainapp/models.py
+++ alwasila/mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название продукта')
+    img = models.ImageField(upload_to='product_img/', verbose_name='Изображение (370 x 479 px)')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
